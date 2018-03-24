5ee7a7d274ae9a85746e478faeeca12d0d49795d
Marty VanHoof <garchuvius@yahoo.ca>
2018-03-24 16:51:57 -0400

    Fix PERF_FORMAT_STRING

--- tester.py
+++ tester.py
@@ -1,11 +1,13 @@
 from sklearn.model_selection import StratifiedShuffleSplit
 from feature_format import featureFormat, targetFeatureSplit
+from IPython.display import display
+import pandas as pd 
 
 PERF_FORMAT_STRING = "\
-\tAccuracy: {:>0.{display_precision}f}\tPrecision: {:>0.{display_precision}f}\t\
-Recall: {:>0.{display_precision}f}\tF1: {:>0.{display_precision}f}\tF2: {:>0.{display_precision}f}"
-RESULTS_FORMAT_STRING = "\tTotal predictions: {:4d}\tTrue positives: {:4d}\tFalse positives: {:4d}\
-\tFalse negatives: {:4d}\n\tTrue negatives: {:4d}"
+Accuracy: {:>0.{display_precision}f}\tPrecision: {:>0.{display_precision}f}\
+\tRecall: {:>0.{display_precision}f}\tF1: {:>0.{display_precision}f}\tF2: {:>0.{display_precision}f}"
+RESULTS_FORMAT_STRING = "Total predictions: {:4d}\tTrue positives: {:4d}\tFalse positives: {:4d}\
+\nFalse negatives: {:4d}\tTrue negatives: {:4d}"
 
 def test_classifier(clf, dataset, feature_list, folds = 1000):
     '''
@@ -55,10 +57,15 @@ def test_classifier(clf, dataset, feature_list, folds = 1000):
         recall = 1.0*true_positives/(true_positives+false_negatives)
         f1 = 2.0 * true_positives/(2*true_positives + false_positives+false_negatives)
         f2 = (1+2.0*2.0) * precision*recall/(4*precision + recall)
-        #print clf
+        #results = dict(accuracy=accuracy, precision=precision, recall=recall, f1=f1, f2=f2,
+        #               total_predictions=total_predictions, true_positives=true_positives,
+        #               false_positives=false_positives, false_negatives=false_negatives,
+        #               true_negatives=true_negatives)
+        #results_df = pd.DataFrame.from_dict(results)
+        #print(results_df)
         print(PERF_FORMAT_STRING.format(accuracy, precision, recall, f1, f2, display_precision = 5))
         print(RESULTS_FORMAT_STRING.format(total_predictions, true_positives, false_positives, false_negatives, true_negatives))
-        #print()
+        print()
     except:
         print("Got a divide by zero when trying out:", clf)
         print("Precision or recall may be undefined due to a lack of true positive predicitons.")
